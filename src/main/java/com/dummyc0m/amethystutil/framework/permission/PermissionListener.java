10698a8d676ef140654c299a3cb814e90ec4b83d
dummy <m.steve.jobs@me.com>
2015-04-09 19:51:26 -0400

    WIP

--- src/main/java/com/dummyc0m/amethystutil/framework/permission/PermissionListener.java
+++ src/main/java/com/dummyc0m/amethystutil/framework/permission/PermissionListener.java
@@ -1,8 +1,11 @@
 package com.dummyc0m.amethystutil.framework.permission;
 
+import org.bukkit.event.Listener;
+
 /**
  * com.dummyc0m.amethystutil.framework.permission
  * Created by Dummyc0m on 4/4/15.
  */
-public class PermissionListener {
+public class PermissionListener implements Listener {
+
 }
