1db0db9865f2fb7af0f816dd06e52db9ffef4afb
Dummyc0m <m.steve.jobs@me.com>
2015-07-08 01:09:46 +0800

    just permissions left

--- src/main/java/com/dummyc0m/amethystcore/AmethystCore.java
+++ src/main/java/com/dummyc0m/amethystcore/AmethystCore.java
@@ -1,16 +1,11 @@
 package com.dummyc0m.amethystcore;
 
 import com.dummyc0m.amethystcore.framework.inventory.InventoryListener;
-import com.dummyc0m.amethystcore.framework.item.ACItemData;
-import com.dummyc0m.amethystcore.framework.item.ACItemManager;
 import com.dummyc0m.amethystcore.framework.item.ItemListener;
 import com.dummyc0m.amethystcore.framework.permission.PermissionListener;
 import com.dummyc0m.amethystcore.framework.region.RegionListener;
-import com.dummyc0m.amethystcore.test.ItemCore;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -23,8 +18,7 @@ import java.util.logging.Logger;
 public class AmethystCore extends JavaPlugin {
     private static AmethystCore AMETHYSTCORE;
     private Logger logger = this.getLogger();
-    private ItemCore itemCore;
-    private String version = "SNAPSHOT 0.1 For 1.8.4";
+    private String version = "1.0-SNAPSHOT For 1.8.7";
 
     public static AmethystCore getInstance() {
         return AMETHYSTCORE;
@@ -38,9 +32,6 @@ public class AmethystCore extends JavaPlugin {
         this.getServer().getPluginManager().registerEvents(new InventoryListener(), this);
         this.getServer().getPluginManager().registerEvents(new PermissionListener(), this);
         this.getServer().getPluginManager().registerEvents(new RegionListener(), this);
-        logger.info("Registering Demo Item");
-        this.itemCore = new ItemCore(new ACItemData(Material.COMPASS), "AmethystCore", "amethystcore:item_core");
-        ACItemManager.getInstance().registerItem(itemCore);
         logger.info("Enabled");
     }
 
@@ -54,10 +45,7 @@ public class AmethystCore extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if ("amethystcore".equalsIgnoreCase(command.getName())) {
-            if (sender instanceof Player && sender.hasPermission("amethystcore.command.item_core")) {
-                ((Player) sender).getInventory().addItem(this.itemCore.getItemStack());
-                return true;
-            }
+            sender.sendMessage("Running AmethystCore Version." + version);
         }
         return false;
     }
--- src/main/java/com/dummyc0m/amethystcore/util/JsonUtil.java
+++
@@ -1,8 +0,0 @@
-package com.dummyc0m.amethystcore.util;
-
-/**
- * Created by Dummyc0m on 7/7/15.
- */
-public class JsonUtil {
-
-}
